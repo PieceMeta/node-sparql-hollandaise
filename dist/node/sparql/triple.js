6e0b3e38fe4ac8e45b136cbb7ec2a84255cf37e8
Anton Koch <anton.koch@gmail.com>
2016-02-04 13:58:59 +0100

    added basic support for object lists and predicate-object lists in triples

--- dist/node/sparql/triple.js
+++ dist/node/sparql/triple.js
@@ -12,25 +12,59 @@ var Triple = function () {
     function Triple() {
         _classCallCheck(this, Triple);
 
-        var splitTriple = [null, null, null];
+        var splitTriple = null;
 
         for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {
             args[_key] = arguments[_key];
         }
 
-        if (args.length === 3) {
-            splitTriple = args;
-        } else if (args.length === 1 && typeof args[0] === 'string') {
-            splitTriple = args[0].split(' ');
+        switch (args.length) {
+            case 3:
+                var valid = true;
+                for (var i = 0; i < args.length; i += 1) {
+                    if (typeof args[i] !== 'string') {
+                        valid = false;
+                    }
+                }
+                if (valid) splitTriple = args;
+                break;
+            case 2:
+                if (typeof args[0] === 'string' && Array.isArray(args[1])) {
+                    var params = args[0].split(' ');
+                    if (params.length === 2) {
+                        // both subject and predicate were given
+                        // we have an object list
+                        splitTriple = [params[0], params[1], args[1]];
+                    } else if (params.length === 1) {
+                        // only subject was given
+                        // this is an object-predicate list
+                        splitTriple = [params[0], args[1], null];
+                    }
+                }
+                break;
+            case 1:
+                if (typeof args[0] === 'string') {
+                    splitTriple = args[0].split(' ');
+                }
+                break;
         }
-        this.subject = splitTriple[0];
-        this.predicate = splitTriple[1];
-        this.object = splitTriple[2];
+        if (Array.isArray(splitTriple)) {
+            this.subject = splitTriple[0];
+            this.predicate = splitTriple[1];
+            this.object = splitTriple[2];
+        } else {
+            throw new Error('Triple: Wrong argument count or malformed input');
+        }
     }
 
     _createClass(Triple, [{
         key: 'toString',
         value: function toString() {
+            if (Array.isArray(this.predicate)) {
+                return this.subject + ' ' + this.predicate.join(' ; ');
+            } else if (Array.isArray(this.object)) {
+                return this.subject + ' ' + this.predicate + ' ' + this.object.join(' , ');
+            }
             return this.subject + ' ' + this.predicate + ' ' + this.object;
         }
     }]);
--- src/sparql/triple.js
+++ src/sparql/triple.js
@@ -2,18 +2,52 @@
 
 export default class Triple {
     constructor(...args) {
-        var splitTriple = [null, null, null];
-        if (args.length === 3) {
-            splitTriple = args;
-        } else if (args.length === 1 && typeof args[0] === 'string') {
-            splitTriple = args[0].split(' ');
+        var splitTriple = null;
+        switch (args.length) {
+            case 3:
+                let valid = true;
+                for (let i = 0; i < args.length; i += 1) {
+                    if (typeof args[i] !== 'string') {
+                        valid = false;
+                    }
+                }
+                if (valid) splitTriple = args;
+                break;
+            case 2:
+                if (typeof args[0] === 'string' && Array.isArray(args[1])) {
+                    let params = args[0].split(' ');
+                    if (params.length === 2) {
+                        // both subject and predicate were given
+                        // we have an object list
+                        splitTriple = [params[0], params[1], args[1]];
+                    } else if (params.length === 1) {
+                        // only subject was given
+                        // this is an object-predicate list
+                        splitTriple = [params[0], args[1], null];
+                    }
+                }
+                break;
+            case 1:
+                if (typeof args[0] === 'string') {
+                    splitTriple = args[0].split(' ');
+                }
+                break;
         }
-        this.subject = splitTriple[0];
-        this.predicate = splitTriple[1];
-        this.object = splitTriple[2];
+        if (Array.isArray(splitTriple)) {
+            this.subject = splitTriple[0];
+            this.predicate = splitTriple[1];
+            this.object = splitTriple[2];
+        } else {
+            throw new Error('Triple: Wrong argument count or malformed input');
+        }
     }
 
     toString() {
+        if (Array.isArray(this.predicate)) {
+            return `${this.subject} ${this.predicate.join(' ; ')}`;
+        } else if (Array.isArray(this.object)) {
+            return `${this.subject} ${this.predicate} ${this.object.join(' , ')}`;
+        }
         return `${this.subject} ${this.predicate} ${this.object}`;
     }
 }
