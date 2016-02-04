cded50756d90666929b83acf77458e2b46871929
Anton Koch <anton.koch@gmail.com>
2016-02-04 15:00:34 +0100

    added prefix-index for prefix 'shortcodes'

---
+++ src/sparql/prefix-index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const PrefixIndex = {
+    'dct': 'http://purl.org/dc/terms/',
+    'foaf': 'http://xmlns.com/foaf/0.1/',
+    'rdf': 'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
+    'rdfs': 'http://www.w3.org/2000/01/rdf-schema#',
+    'rel': 'http://www.perceive.net/schemas/relationship/',
+    'xsd': 'http://www.w3.org/2001/XMLSchema#'
+};
+
+export default PrefixIndex;
--- src/sparql/prefix.js
+++ src/sparql/prefix.js
@@ -1,8 +1,21 @@
 'use strict';
 
+import PrefixIndex from './prefix-index';
+
 export default class Prefix {
-    constructor(value) {
-        this.value = value.replace(/^PREFIX /, '');
+    constructor(value, prefixIndex = null) {
+        if (prefixIndex === null) {
+            prefixIndex = PrefixIndex;
+        }
+        if (value.indexOf(':') === -1) {
+            if (prefixIndex.hasOwnProperty(value)) {
+                this.value = `${value}: <${prefixIndex[value]}>`;
+            } else {
+                throw Error(`No prefix found in PrefixIndex matching: ${value}`);
+            }
+        } else {
+            this.value = value.replace(/^PREFIX /, '');
+        }
     }
 
     toString() {
