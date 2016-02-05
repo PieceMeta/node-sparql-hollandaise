46edc42d235e6a0d9e4c9644d8e9c80997a01ffd
Anton Koch <anton.koch@gmail.com>
2016-02-05 17:45:53 +0100

    initial gh-pages branch

---
+++ doc/api.js
@@ -0,0 +1,22 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Ask",
+        "Construct",
+        "Describe",
+        "Filter",
+        "GraphPattern",
+        "GroupGraphPattern",
+        "Prefix",
+        "PrefixIndex",
+        "Query",
+        "Result",
+        "Select",
+        "Transport",
+        "Triple"
+    ],
+    "modules": [],
+    "allModules": [],
+    "elements": []
+} };
+});
